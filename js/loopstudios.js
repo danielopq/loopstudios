65469f6509b9e758bb64f3fd355e10ae0ff08205
Daniel Martínez Duque <danielopq@gmail.com>
2024-03-16 19:32:08 +0000

    final version done

--- js/loopstudios.js
+++ js/loopstudios.js
@@ -1,1 +1,16 @@
-// JavaScript Document
+window.onload = start;
+
+function start(){
+	document.getElementById("menu-hamburger").addEventListener('click',showMobileMenu,false);
+	document.getElementById("close-icon").addEventListener('click',hideMobileMenu,false);
+}
+
+function showMobileMenu(){
+	document.getElementById("menu-mobile").style.display = "block";
+	document.getElementsByTagName("html")[0].style.overflow = "hidden";
+}
+
+function hideMobileMenu(){
+	document.getElementById("menu-mobile").style.display = "none";
+	document.getElementsByTagName("html")[0].style.overflow = "auto";
+}
