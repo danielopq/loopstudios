46a983664123325769f9541069dd25ddf066b5f9
danielopq <danielopq@gmail.com>
2024-08-04 16:34:25 +0100

    loopstudios.js - cleaning code, JSDoc added

--- js/loopstudios.js
+++ js/loopstudios.js
@@ -1,15 +1,25 @@
-window.onload = start;
+/**
+ * @fileoverview Contains de main acctions do hide or display the main menu while the mobile version is running.
+ * @author Daniel Martinez Duque
+ * @date 2024-08-04
+ */
 
-function start(){
+(()=>{
 	document.getElementById("menu-hamburger").addEventListener('click',showMobileMenu,false);
 	document.getElementById("close-icon").addEventListener('click',hideMobileMenu,false);
-}
+})()
 
+/**
+ * Displays the main menu (mobile version).
+ */
 function showMobileMenu(){
 	document.getElementById("menu-mobile").style.display = "block";
 	document.getElementsByTagName("html")[0].style.overflow = "hidden";
 }
 
+/**
+ * Hide the main menu (mobile version).
+ */
 function hideMobileMenu(){
 	document.getElementById("menu-mobile").style.display = "none";
 	document.getElementsByTagName("html")[0].style.overflow = "auto";
